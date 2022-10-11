a8d96170e9a19a6c9fb67bfa90a20299350edbc9
Dmytro Shepeliuk <westcool@gmail.com>
2022-10-11 12:24:19 +0300

    changes

--- index.js
+++ index.js
@@ -173,8 +173,8 @@ process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 // Telegram things
 process.on('unhandledRejection', (error) => {
-  console.error(error)
+  //console.error(error)
 })
 process.on('uncaughtException', (error) => {
-  console.error(error)
+  //console.error(error)
 })
